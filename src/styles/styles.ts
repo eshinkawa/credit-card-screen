3f2037dfb99b64833f30060c69d9c77215082b13
Eduardo Shinkawa <eshinkawa@gmail.com>
2023-05-17 03:10:47 -0300

    feat: add styles

---
+++ src/styles/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    paddingTop: 72,
+    backgroundColor: '#D6D6D6'
+  },
+  input: {
+    marginBottom: 10
+  }
+});
