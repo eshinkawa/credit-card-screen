792ad7cb2e8d32f0aa2e31138dd60cabafbb4caf
Eduardo Shinkawa <eshinkawa@gmail.com>
2023-05-17 03:10:19 -0300

    feat: add expiration date component

---
+++ src/components/ExpirationDate/ExpirationDate.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ExpirationDate from './ExpirationDate';
+import { fireEvent, render } from '@testing-library/react-native';
+
+test('ExpirationDate component renders correctly', () => {
+  const setExpirationDate = jest.fn();
+  const setExpirationDateTouch = jest.fn();
+  const { getByTestId, getAllByText } = render(
+    <ExpirationDate
+      expirationDate="12/22"
+      setExpirationDate={setExpirationDate}
+      expirationDateTouch={false}
+      setExpirationDateTouch={setExpirationDateTouch}
+    />
+  );
+  const input = getByTestId('expiration-date');
+  expect(input).toBeTruthy();
+  fireEvent(input, 'blur');
+  fireEvent.changeText(input, '12/22');
+  expect(setExpirationDate).toHaveBeenCalledWith('12/22');
+  expect(input.props.value).toBe('12/22');
+  expect(setExpirationDateTouch).toHaveBeenCalledWith(true);
+});
---
+++ src/components/ExpirationDate/ExpirationDate.tsx
@@ -0,0 +1,44 @@
+import React, { useState, FC } from 'react';
+import { View, TextInputProps } from 'react-native';
+import { TextInput, Button, HelperText } from 'react-native-paper';
+import { isValidCardNumber, isValidExpirationDate } from '../../domain/utils';
+import { styles } from '../../styles/styles';
+
+type ExpirationDateProps = {
+  expirationDate: string;
+  setExpirationDate: (text: string) => void;
+  expirationDateTouch: boolean;
+  setExpirationDateTouch: (touch: boolean) => void;
+}
+
+const ExpirationDate: FC<ExpirationDateProps> = ({ expirationDate, setExpirationDate, expirationDateTouch, setExpirationDateTouch }) => {
+
+  const handleExpirationDateChange = (text: string) => {
+    let formattedText = text;
+    if (text.length === 2 && !text.includes('/')) {
+      formattedText = `${text}/`;
+    }
+    setExpirationDate(formattedText);
+  };
+  return (
+    <>
+      <TextInput
+        testID="expiration-date"
+        label="Expiration Date (MM/YY)"
+        value={expirationDate}
+        onChangeText={handleExpirationDateChange}
+        keyboardType="numeric"
+        maxLength={5}
+        style={[styles.input, {width: '100%'}]}
+        error={expirationDateTouch && !isValidExpirationDate(expirationDate)}
+        onBlur={() => setExpirationDateTouch(true)}
+      />
+      {!isValidExpirationDate(expirationDate) && expirationDateTouch && (
+        <HelperText type="error" visible={true}>
+          Invalid Expiration Date
+        </HelperText>
+      )}
+    </>);
+};
+
+export default ExpirationDate;
